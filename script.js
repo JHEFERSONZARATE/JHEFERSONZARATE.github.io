d6dcd65d806eb2e5988cc8803eaac82a42ae3c42
Jheferson <jhefersonzaratefernandez@gmail.com>
2024-03-15 22:17:50 -0500

    Mi Web

---
+++ script.js
@@ -0,0 +1,43 @@
+let menuVisible=false;
+//funcion que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList="responsive";
+        menuVisible = true;
+    }
+}
+function seleccionar(){
+    //oculta el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList="";
+    menuVisible=false;
+}
+//funcion que aplica las animaciones de habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("java");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("c");
+        habilidades[3].classList.add("work");
+        habilidades[4].classList.add("powerpoint");
+        habilidades[5].classList.add("powerbi");
+        habilidades[6].classList.add("excel");
+        habilidades[7].classList.add("comunicacion");
+        habilidades[8].classList.add("tre");
+        habilidades[9].classList.add("creatividad");
+        habilidades[10].classList.add("dedicacion");
+        habilidades[11].classList.add("management");
+    }
+}
+//detecta el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll=function(){
+    efectoHabilidades();
+}
+function redireccionar(){
+    window.open("https://drive.google.com/drive/folders/1QVrsqWzBj-PD19dSTGh4skiCvPR56RIz?usp=drive_link");
+}
